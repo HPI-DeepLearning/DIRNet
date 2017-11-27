1e9e6f053f91f700be440711f60c9ff80a6ec602
adrianloy <adrianloy@student.hpi.de>
2017-11-27 23:08:55 +0100

    Added concatenation layer and spatial transformer layer to the net.

--- convnet.py
+++ convnet.py
@@ -9,9 +9,12 @@ import numpy as np
 
 def conv_net_regressor(image_shape, bn_mom=0.9):
     (nchannel, height, width) = image_shape
-    data = mx.sym.Variable(name='data')
-    data = mx.sym.BatchNorm(data=data, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='bn_data')
-    # Thenumber of kernels per layer can be of arbitrary size, but the number of kernels of the output layer is
+    # We have 2 data sources and concatenate them
+    data_fixed = mx.sym.Variable(name='data_fixed')
+    data_moving = mx.sym.Variable(name='data_moving')
+    concat_data = mx.sym.concat(*[data_fixed, data_moving])
+    data = mx.sym.BatchNorm(data=concat_data, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='bn_data')
+    # The number of kernels per layer can be of arbitrary size, but the number of kernels of the output layer is
     # determined by the dimensionality of the input images
     filter_list = [16, 64, 128, 256]
     # four alternating layers of 3 × 3 convolutions with 0-padding and 2 × 2 downsampling layers
@@ -32,6 +35,9 @@ def conv_net_regressor(image_shape, bn_mom=0.9):
         body = mx.sym.Activation(data=body, act_type='relu', name='relu' + str(i))
         body = mx.sym.Pooling(data=body, kernel=(2, 2), stride=(2, 2), pad=(1, 1), pool_type='avg')
 
+    # Nicht sicher ob loc einfach der output vom convnet regressor ist.
+    # Ich vermute man muss die  anzahl filter und kernel sizes so anpassen dass das irgendwie zusammen passt
+    body = mx.sym.SpatialTransformer(data=data_moving, loc=body)
 
 def get_symbol(image_shape):
-    return conv_net_regressor()
+    return conv_net_regressor(image_shape)
