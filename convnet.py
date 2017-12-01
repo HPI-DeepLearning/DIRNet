d43a888149a4742823a847bef1fd8ff92f98ccec
adrianloy <adrianloy@student.hpi.de>
2017-12-01 16:33:01 +0100

    Added iterators, solved some bugs in the network architecture. Added data dir

--- convnet.py
+++ convnet.py
@@ -5,6 +5,51 @@ https://arxiv.org/abs/1704.06065
 
 import mxnet as mx
 import numpy as np
+import gzip
+import struct
+
+
+def get_mnist(mnistdir='/data/'):
+    def read_data(label_url, image_url):
+        with gzip.open(label_url) as flbl:
+            struct.unpack(">II", flbl.read(8))
+            label = np.fromstring(flbl.read(), dtype=np.int8)
+        with gzip.open(image_url, 'rb') as fimg:
+            _, _, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            image = np.fromstring(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
+            image = image.reshape(image.shape[0], 1, 28, 28).astype(np.float32) / 255
+        return label, image
+
+    (train_lbl, train_img) = read_data(
+        mnistdir + 'train-labels-idx1-ubyte.gz', mnistdir + 'train-images-idx3-ubyte.gz')
+    (test_lbl, test_img) = read_data(
+        mnistdir + 't10k-labels-idx1-ubyte.gz', mnistdir + 't10k-images-idx3-ubyte.gz')
+    return {'train_data': train_img, 'train_label': train_lbl,
+            'test_data': test_img, 'test_label': test_lbl}
+
+
+def get_mnist_data_iterator(mnistdir='./data/', digit=1):
+    def get_iterator_single_digit(data, label):
+        one_digit_indices = []  # Contains all indices with images depicting the digit
+        for index in range(len(label)):  # There might be a faster way to do this
+            if label[index] == digit:
+                one_digit_indices.append(index)
+        one_digit_data = data[one_digit_indices]
+        one_digit_label = label[one_digit_indices]
+        fixed_image = one_digit_data[np.random.randint(0, len(one_digit_label))]
+        one_digit_fixed_image = []  # array of same length as above data array, but its the same img multiple times
+        for _ in one_digit_data:
+            one_digit_fixed_image.append(fixed_image)
+
+        iterator = mx.io.NDArrayIter([one_digit_fixed_image, one_digit_data],
+                                 [one_digit_label, one_digit_label],
+                                 batch_size=1, shuffle=True)
+        return iterator
+
+    mnist = get_mnist(mnistdir)
+    train_iter = get_iterator_single_digit(mnist['train_data'], mnist['train_label'])
+    val_iter = get_iterator_single_digit(mnist['test_data'], mnist['test_label'])
+    return train_iter, val_iter
 
 
 def conv_net_regressor(image_shape, bn_mom=0.9):
@@ -13,21 +58,25 @@ def conv_net_regressor(image_shape, bn_mom=0.9):
     data_fixed = mx.sym.Variable(name='data_fixed')
     data_moving = mx.sym.Variable(name='data_moving')
     concat_data = mx.sym.concat(*[data_fixed, data_moving])
-    data = mx.sym.BatchNorm(data=concat_data, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='bn_data')
+    batched = mx.sym.BatchNorm(data=concat_data, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='bn_data')
     # The number of kernels per layer can be of arbitrary size, but the number of kernels of the output layer is
     # determined by the dimensionality of the input images
-    filter_list = [16, 64, 128, 256]
+    filter_list = [16, 32, 64, 128]
     # four alternating layers of 3 × 3 convolutions with 0-padding and 2 × 2 downsampling layers
     for i in range(4):
-        body = mx.sym.Convolution(data=data, num_filter=filter_list[i], kernel=(3, 3), stride=(2, 2), pad=(0, 0),
-                                  no_bias=True, name="conv" + str(i))
+        if i == 0:
+            body = mx.sym.Convolution(data=batched, num_filter=filter_list[i], kernel=(3, 3), stride=(2, 2), pad=(0, 0),
+                                      no_bias=True, name="conv" + str(i))
+        else:
+            body = mx.sym.Convolution(data=body, num_filter=filter_list[i], kernel=(3, 3), stride=(2, 2), pad=(0, 0),
+                                      no_bias=True, name="conv" + str(i))
         body = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=2e-5, momentum=bn_mom, name='bn' + str(i))
         # TO DO: the original authors use exponential linear units as activation
         body = mx.sym.Activation(data=body, act_type='relu', name='relu' + str(i))
         body = mx.sym.Pooling(data=body, kernel=(2, 2), stride=(2, 2), pad=(1, 1), pool_type='avg')
     # Subsequently, three 1 × 1 convolutional layers are applied to make the ConvNet regressor fully convolutional
     for k in range(3):
-        i = k+4
+        i = k + 4
         body = mx.sym.Convolution(data=body, num_filter=256, kernel=(1, 1), stride=(1, 1), pad=(0, 0),
                                   no_bias=True, name="conv" + str(i))
         body = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=2e-5, momentum=bn_mom, name='bn' + str(i))
@@ -36,14 +85,29 @@ def conv_net_regressor(image_shape, bn_mom=0.9):
         body = mx.sym.Pooling(data=body, kernel=(2, 2), stride=(2, 2), pad=(1, 1), pool_type='avg')
         #
         flatten = mx.sym.flatten(data=body)
-        fc3 = mx.sym.FullyConnected(data=flatten, num_hidden=6) # Todo: Initialize as identity
+        fc3 = mx.sym.FullyConnected(data=flatten, num_hidden=6)  # Todo: Initialize as identity
     # The Spatial Transformer performs a affine transformation to the moving image,
     # parametrized by the output of the body network
-    stnet = mx.sym.SpatialTransformer(data=data_moving, loc=fc3, target_shape=image_shape, type='Affine',
-                                     sampler_type="bilinear", name='SpatialTransformer')
-    cor = mx.sym.Correlation(data1=data_fixed, data2=stnet, )
+    stnet = mx.sym.SpatialTransformer(data=data_moving, loc=fc3, target_shape=image_shape, transform_type='affine',
+                                      sampler_type="bilinear", name='SpatialTransformer')
+    Y = mx.symbol.Variable('lin_reg_label')
+    cor = mx.sym.Correlation(data1=data_fixed, data2=stnet)
     stnet = mx.sym.MakeLoss(cor, normalization='batch')
     return stnet
 
+
 def get_symbol(image_shape):
     return conv_net_regressor(image_shape)
+
+
+if __name__ == '__main__':
+    mnist_shape = (1,28,28)
+    iterators = get_mnist_data_iterator()
+    model = mx.mod.Module(symbol=get_symbol(mnist_shape), context=mx.gpu(),
+                          label_names= ,data_names=['data_fixed', 'data_moving'])
+    model.fit(iterators[0],  # eval_data=val_iter,
+                    optimizer='sgd',
+                    optimizer_params={'learning_rate': 0.1},
+                    #eval_metric=eval_metrics,#mx.metric.Loss(),#'acc',
+                    #batch_end_callback = mx.callback.Speedometer(batch_size, 100),
+                    num_epoch=10)
